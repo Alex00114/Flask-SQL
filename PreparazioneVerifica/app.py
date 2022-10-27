e71783ad2e9eaabd222a12f8bb067366e7188856
Alex00114 <tag.alessandro@iisgalvanimi.edu.it>
2022-10-27 19:42:23 +0000

    finita PreparazioneVerifica

--- PreparazioneVerifica/app.py
+++ PreparazioneVerifica/app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template, request,  send_file, make_response, url_for, Response, redirect
+from flask import Flask, render_template, request, send_file, make_response, url_for, Response, redirect
 app = Flask(__name__)
-import pandas as pd, pymssql
+import io
+import geopandas
+import contextily
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import pandas as pd
+import pymssql
+conn = pymssql.connect(server = '213.140.22.237\SQLEXPRESS', user='tag.alessandro', password='xxx123##', database='tag.alessandro')
 
 
 @app.route('/', methods=['GET'])
@@ -17,13 +27,12 @@ def selezione():
     elif scelta == "Es3":
         return redirect(url_for("ProdottiBrand"))
     else:
-        return redirect(url_for("input"))
+        return render_template('input.html')
 
 @app.route('/numeroProdotti', methods=['GET'])
 def numeroProdotti():
     global dfNumeroProdotti
-    conn = pymssql.connect(server = '213.140.22.237\SQLEXPRESS', user='tag.alessandro', password='xxx123##', database='tag.alessandro')
-
+    
     query = 'select category_name, count(*) as prodotti_per_categoria from production.categories inner join production.products on production.categories.category_id = production.products.category_id group by category_name order by prodotti_per_categoria desc'
     dfNumeroProdotti = pd.read_sql(query, conn)
     return render_template('numeroProdotti.html', nomiColonne = dfNumeroProdotti.columns.values, dati = list(dfNumeroProdotti.values.tolist()))
@@ -40,12 +49,56 @@ def grafico():
     ax.set_ylabel('Categorie',color = "black")
 
     plt.rcParams.update({"font.size": 12 })
-    fig.autofmt_xdate(rotation=45) ##ruotare le scritte sull asse x del grafico
+    fig.autofmt_xdate(rotation=45)
 
     # visualizzazione grafico:
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
     return Response(output.getvalue(), mimetype='image/png')
+
+@app.route('/numeroOrdini', methods=['GET'])
+def numeroOrdini():
+    global dfNumeroOrdini
+
+    query ='select store_name, count(*)as ordini_per_store from sales.orders inner join sales.stores on sales.orders.store_id = sales.stores.store_id group by store_name order by ordini_per_store desc'
+    dfNumeroOrdini = pd.read_sql(query, conn)
+    return render_template('numeroOrdini.html', nomiColonne = dfNumeroOrdini.columns.values, dati = list(dfNumeroOrdini.values.tolist()))
+
+@app.route('/grafico2', methods=['GET'])
+def grafico2():
+    fig, ax = plt.subplots(figsize = (12,6))
+
+    ax.barh(dfNumeroOrdini["store_name"], dfNumeroOrdini['ordini_per_store'], color = "c")
+
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+@app.route('/ProdottiBrand', methods=['GET'])
+def ProdottiBrand():
+    global dfProdottiBrand
+
+    query ='select brand_name, count(*)as protti_per_brand from production.brands inner join production.products on production.products.brand_id = production.brands.brand_id group by brand_name order by protti_per_brand desc'
+    dfProdottiBrand = pd.read_sql(query, conn)
+    return render_template('prodottiBrand.html', nomiColonne = dfProdottiBrand.columns.values, dati = list(dfProdottiBrand.values.tolist()))
+
+@app.route('/grafico3', methods=['GET'])
+def grafico3():
+    fig, ax = plt.subplots(figsize = (15,9))
+
+    ax.pie(dfProdottiBrand['protti_per_brand'], labels = dfProdottiBrand['brand_name'])
+
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+@app.route('/result', methods=['GET'])
+def result():
+    NomeProdotto = request.args['NomeProdotto']
+    query = f"select * from production.products where product_name LIKE '{NomeProdotto}%'"
+    dfprodotti = pd.read_sql(query, conn)
+    # visualizzare le informazioni 
+    return render_template('result.html', nomiColonne = dfprodotti.columns.values, dati = list(dfprodotti.values.tolist()))
 
 
 if __name__ == '__main__':
